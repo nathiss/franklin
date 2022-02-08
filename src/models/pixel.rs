732fe27494ddaaefb467f634e746c519277326e6
nathiss <kamil.jakub.rusin@gmail.com>
2022-02-08 19:18:14 +0100

    Add UTs for Pixel struct

--- src/models/pixel.rs
+++ src/models/pixel.rs
@@ -52,3 +52,83 @@ impl Pixel {
         [self.r, self.g, self.b]
     }
 }
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn white_createAnInstance_allFieldsSetToMaxValue() {
+        let white = Pixel::white();
+
+        assert_eq!(255, white.get_r());
+        assert_eq!(255, white.get_g());
+        assert_eq!(255, white.get_b());
+    }
+
+    #[test]
+    fn grayscale_withCustomValue_allFieldsSetToCorrectValue() {
+        let grayscale = Pixel::grayscale(127);
+
+        assert_eq!(127, grayscale.get_r());
+        assert_eq!(127, grayscale.get_g());
+        assert_eq!(127, grayscale.get_b());
+    }
+
+    #[test]
+    fn new_withCustomValues_allFieldsSetToCorrectValues() {
+        let pixel = Pixel::new(125, 126, 127);
+
+        assert_eq!(125, pixel.get_r());
+        assert_eq!(126, pixel.get_g());
+        assert_eq!(127, pixel.get_b());
+    }
+
+    #[test]
+    fn r_withDifferentValue_valueOfRedChangedAccordingly() {
+        let mut pixel = Pixel::new(125, 126, 127);
+
+        pixel.r(128);
+
+        assert_eq!(128, pixel.get_r());
+    }
+
+    #[test]
+    fn g_withDifferentValue_valueOfGreenChangedAccordingly() {
+        let mut pixel = Pixel::new(125, 126, 127);
+
+        pixel.g(128);
+
+        assert_eq!(128, pixel.get_g());
+    }
+
+    #[test]
+    fn b_withDifferentValue_valueOfBlueChangedAccordingly() {
+        let mut pixel = Pixel::new(125, 126, 127);
+
+        pixel.b(128);
+
+        assert_eq!(128, pixel.get_b());
+    }
+
+    #[test]
+    fn set_grayscale_withDifferentValue_allFieldsSetToCorrectValues() {
+        let mut pixel = Pixel::new(125, 126, 127);
+
+        pixel.set_grayscale(128);
+
+        assert_eq!(128, pixel.get_r());
+        assert_eq!(128, pixel.get_g());
+        assert_eq!(128, pixel.get_b());
+    }
+
+    #[test]
+    fn as_slice_fieldsWithDifferentValues_returnsSliceWithCorrectValues() {
+        let pixel = Pixel::new(125, 126, 127);
+
+        let slice = pixel.as_slice();
+
+        assert_eq!([125, 126, 127], slice);
+    }
+}
