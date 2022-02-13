f46860a6c9d8cb48c66b1f828432be1bef105f2d
nathiss <kamil.jakub.rusin@gmail.com>
2022-02-13 16:36:43 +0100

    Refactor TriangleMutator
    * Extract common code of mutate_rgb and mutate_grayscale into methods

--- src/mutators/triangle_mutator.rs
+++ src/mutators/triangle_mutator.rs
@@ -1,6 +1,17 @@
-use crate::{util::Random, models::Image};
+use crate::{
+    models::{Image, Pixel},
+    util::Random,
+};
 
 use super::Mutator;
+
+fn get_dx(vertices: &[Point; 3]) -> (f64, f64, f64) {
+    (
+        (vertices[2].x - vertices[0].x) as f64 / (vertices[2].y - vertices[0].y + 1) as f64,
+        (vertices[1].x - vertices[0].x) as f64 / (vertices[1].y - vertices[0].y + 1) as f64,
+        (vertices[2].x - vertices[1].x) as f64 / (vertices[2].y - vertices[1].y + 1) as f64,
+    )
+}
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 struct Point {
@@ -21,7 +32,7 @@ impl TriangleMutator {
         Point { x, y }
     }
 
-    fn get_triangle_vertices(&mut self, image: &Image) -> (Point, Point, Point) {
+    fn get_triangle_vertices(&mut self, image: &Image) -> [Point; 3] {
         let mut vertices = Vec::new();
 
         while vertices.len() != 3 {
@@ -35,106 +46,91 @@ impl TriangleMutator {
         // The points must be sorted vertically. Required by the algorithm.
         vertices.sort_by(|p1, p2| p1.y.cmp(&p2.y));
 
-        (vertices[0], vertices[1], vertices[2])
+        [vertices[0], vertices[1], vertices[2]]
+    }
+
+    fn draw_triangle<F>(&mut self, image: &mut Image, pixel_mutator: F)
+    where
+        F: Fn(&mut Pixel),
+    {
+        let vertices = self.get_triangle_vertices(image);
+
+        let (dx_far, dx_upper, dx_low) = get_dx(&vertices);
+
+        let image_width = image.width() as isize;
+        let image_height = image.height() as isize;
+
+        let mut xf = vertices[0].x as f64;
+        let mut xt = vertices[0].x as f64 + dx_upper;
+
+        let mut y = vertices[0].y as isize;
+        while y
+            <= if vertices[2].y > image_height - 1 {
+                image_height - 1
+            } else {
+                vertices[2].y
+            }
+        {
+            if y >= 0 {
+                let mut x = if xf > 0f64 { xf as isize } else { 0 };
+                while x
+                    <= if xt < image_width as f64 {
+                        xt as isize
+                    } else {
+                        image_width - 1
+                    }
+                {
+                    let idx = (y * image_width + x) as usize;
+                    pixel_mutator(&mut image[idx]);
+
+                    x += 1;
+                }
+
+                let mut x = if xf < image_width as f64 {
+                    xf as isize
+                } else {
+                    image_width - 1
+                };
+                while x >= if xt > 0f64 { xt as isize } else { 0 } {
+                    let idx = (y * image_width + x) as usize;
+                    pixel_mutator(&mut image[idx]);
+
+                    x -= 1;
+                }
+            }
+            xf += dx_far;
+
+            if y < vertices[1].y {
+                xt += dx_upper;
+            } else {
+                xt += dx_low;
+            }
+
+            y += 1;
+        }
     }
 }
 
 impl Mutator for TriangleMutator {
     fn mutate_rgb(&mut self, image: &mut Image) {
-        let (p0, p1, p2) = self.get_triangle_vertices(image);
-
         let r = self.random.get_random(0u8, 255);
         let g = self.random.get_random(0u8, 255);
         let b = self.random.get_random(0u8, 255);
 
-        let image_width = image.width() as isize;
-        let image_height = image.height() as isize;
+        let rgb_pixel_mutator = move |p: &mut Pixel| {
+            p.r(r);
+            p.g(g);
+            p.b(b);
+        };
 
-        let dx_far = (p2.x - p0.x) as f64 / (p2.y - p0.y + 1) as f64;
-        let dx_upper = (p1.x - p0.x) as f64 / (p1.y - p0.y + 1) as f64;
-        let dx_low = (p2.x - p1.x) as f64 / (p2.y - p1.y +1) as f64;
-
-        let mut xf = p0.x as f64;
-        let mut xt = p0.x as f64 + dx_upper;
-
-        let mut y = p0.y as isize;
-        while y <= if p2.y > image_height - 1 {image_height - 1} else {p2.y} {
-            if y >= 0 {
-                let mut x = if xf > 0f64 {xf as isize} else {0};
-                while x <= if xt < image_width as f64 {xt as isize} else {image_width - 1} {
-                    let idx = (y * image_width + x) as usize;
-                    image[idx].r(r);
-                    image[idx].g(g);
-                    image[idx].b(b);
-
-                    x += 1;
-                }
-
-                let mut x = if xf < image_width as f64 {xf as isize} else {image_width - 1};
-                while x >= if xt > 0f64 {xt as isize} else {0} {
-                    let idx = (y * image_width + x) as usize;
-                    image[idx].r(r);
-                    image[idx].g(g);
-                    image[idx].b(b);
-
-                    x -= 1;
-                }
-            }
-            xf += dx_far;
-
-            if y < p1.y {
-                xt += dx_upper;
-            } else {
-                xt += dx_low;
-            }
-
-            y += 1;
-        }
+        self.draw_triangle(image, rgb_pixel_mutator);
     }
 
     fn mutate_grayscale(&mut self, image: &mut Image) {
-        let (p0, p1, p2) = self.get_triangle_vertices(image);
-
         let grayscale = self.random.get_random(0u8, 255);
 
-        let image_width = image.width() as isize;
-        let image_height = image.height() as isize;
+        let grayscale_pixel_mutator = move |p: &mut Pixel| p.set_grayscale(grayscale);
 
-        let dx_far = (p2.x - p0.x) as f64 / (p2.y - p0.y + 1) as f64;
-        let dx_upper = (p1.x - p0.x) as f64 / (p1.y - p0.y + 1) as f64;
-        let dx_low = (p2.x - p1.x) as f64 / (p2.y - p1.y +1) as f64;
-
-        let mut xf = p0.x as f64;
-        let mut xt = p0.x as f64 + dx_upper;
-
-        let mut y = p0.y as isize;
-        while y <= if p2.y > image_height - 1 {image_height - 1} else {p2.y} {
-            if y >= 0 {
-                let mut x = if xf > 0f64 {xf as isize} else {0};
-                while x <= if xt < image_width as f64 {xt as isize} else {image_width - 1} {
-                    let idx = (y * image_width + x) as usize;
-                    image[idx].set_grayscale(grayscale);
-
-                    x += 1;
-                }
-
-                let mut x = if xf < image_width as f64 {xf as isize} else {image_width - 1};
-                while x >= if xt > 0f64 {xt as isize} else {0} {
-                    let idx = (y * image_width + x) as usize;
-                    image[idx].set_grayscale(grayscale);
-
-                    x -= 1;
-                }
-            }
-            xf += dx_far;
-
-            if y < p1.y {
-                xt += dx_upper;
-            } else {
-                xt += dx_low;
-            }
-
-            y += 1;
-        }
+        self.draw_triangle(image, grayscale_pixel_mutator);
     }
 }
